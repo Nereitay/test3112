ab0cf0e0f3c3ec1917c87502b095a8765acaad7c
nereitay <nereaping@hotmail.com>
2021-05-19 19:48:35 +0200

    Revert "config"
    This reverts commit 29cee54590040656889659c7eeb5195ff2812956.

--- src/main/java/com/springboot/atm/repository/SysLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.atm.repository;
-
-import com.springboot.atm.model.SysLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SysLogRepository extends JpaRepository<SysLog, Integer> {
-}
